9123604a69b31e003817227c1a14e61b12e6f975
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-10-18 01:43:57 +0300

    task2

--- src/ru/croc/task1/Main.java
+++ src/ru/croc/task1/Main.java
@@ -7,17 +7,28 @@ public class Main {
         double x;
         double y;
 
+        // раастояние между двумя точками
         double getDistance(Point a){
             return Math.sqrt((x-a.x)*(x-a.x)+(y-a.y)*(y-a.y));
         }
     }
 
+    // поиск площади (использую формулу Герона)
     static double square(Point a, Point b, Point c){
         double ab_side, ac_side, bc_side;
+        // вычисление всех трех сторон
         ab_side = a.getDistance(b);
         ac_side = a.getDistance(c);
         bc_side = b.getDistance(c);
+
+        // проверка треугольника на существование
+        if((ab_side+ac_side <= bc_side)||(ab_side+bc_side<=ac_side)||(bc_side+ac_side)<=ab_side){
+            return -1;
+        }
+
+        // вычисление полупериметра
         double semi_perimentr = (ab_side + ac_side + bc_side)/2;
+        // по формуле Герона вычисляем площадь
         double result = Math.sqrt(semi_perimentr*(semi_perimentr-ab_side)*(semi_perimentr-ac_side)*(semi_perimentr-bc_side));
         return result;
     }
@@ -25,6 +36,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // ввод координат вершин
         Point a = new Point();
         System.out.print("Введите координату х вершины №1: ");
         a.x = input.nextDouble();
@@ -43,7 +55,10 @@ public class Main {
         System.out.print("Введите координату y вершины №3: ");
         c.y = input.nextDouble();
 
-        System.out.println("Площадь треугольника равна: " + square(a, b, c));
+        // обработка ошибки
+        double result = square(a, b, c);
+        if(result == -1) System.out.println("Треугольник с заданными координатами вершин не существует!");
+        else System.out.println("Площадь треугольника равна: " + result);
         // ...
     }
 }
---
+++ src/ru/croc/task2/task2.java
@@ -0,0 +1,23 @@
+package ru.croc.task2;
+import java.util.Scanner;
+
+public class task2 {
+    static void printBytes(double number){
+
+        // массив ед. измерения
+        String byte_size[] = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+        int k=0;
+        while(number/1024.0 >= 1){
+            k++;
+            number /= 1024.0;
+        }
+        // вывод в "человеко-читаемом" формате
+        System.out.println(String.format("%.1f", number)+" "+byte_size[k]);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        printBytes(input.nextDouble());
+    }
+
+}
