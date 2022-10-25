b16ee6b7b038c97401f9d2ecbb1a3954149ad222
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-10-26 01:31:17 +0300

    fix task_4

--- src/ru/croc/task4/Main.java
+++ src/ru/croc/task4/Main.java
@@ -5,36 +5,64 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String source;
         String noComments;
 
-        while(true){
-            source = in.nextLine();
+
+        final String testSourse =
+        "/*\n"+
+         "* My first ever program in Java!\n"+
+         "*/\n"+
+        "class Hello { // class body starts here\n"+
+        " \n"+
+        "    /* main method */\n"+
+        "    public static void main(String[] args/* we put command line arguments here*/) {\n"+
+        "        // this line prints my first greeting to the screen\n"+
+        "        System.out.println(\"Hi!\"); // :)\n"+
+        "    }\n"+
+        "} // the end\n"+
+        "// to be continued...";
+
+        noComments=removeJavaComments(testSourse);
+        System.out.println(noComments);
+
+        // ввод построчно до тех пор, пока пользователь не введет слово "стоп"
+        /*
+        String source="";
+        source = in.nextLine();
+        while(!source.equals("stop")){
             noComments = removeJavaComments(source);
             System.out.println(noComments);
+            source = in.nextLine();
         }
+        */
     }
 
     private static String removeJavaComments(String source){
         String output = "";
         boolean flag1 = false; // флаг, отвечающий за наличие в строке символа '/'
         boolean flag2 = false; // флаг, отвечающий за наличие в строке символа '*'
-        for(int i=0; i<source.length(); i++){
-            if(source.charAt(i) == '/'){
-                if(flag2 && flag1){ // проверка, заканчивается ли комментарий ("*/")
-                    flag1 = false;
-                    flag2 = false;
-                    continue;
+        String[] lines = source.split("\n");
+        for(int j=0; j<lines.length; j++){
+            for(int i=0; i<lines[j].length(); i++){
+                if(lines[j].charAt(i) == '/'){
+                    if(flag2 && flag1){ // проверка, заканчивается ли комментарий ("*/")
+                        flag1 = false;
+                        flag2 = false;
+                        continue;
+                    }
+                    if(flag1) break; // проверка, является ли комментарий однострочным ("//")
+                    flag1 = true;
                 }
-                if(flag1) break; // проверка, является ли комментарий однострочным ("//")
-                flag1 = true;
+                else if(lines[j].charAt(i) == '*'){
+                    flag2 = true;
+                }
+                else if(!flag1 && !flag2){
+                    output += lines[j].charAt(i);
+                }
             }
-            else if(source.charAt(i) == '*'){
-                flag2 = true;
-            }
-            else if(!flag1 && !flag2){
-                output += source.charAt(i);
-            }
+            if (j != lines.length) output += "\n";
+            flag1 = false;
+            flag2 = false;
         }
         return output;
     }
