932bd9e0da7da91e4c55147b5aaf8ab1c089c011
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-23 16:33:16 +0300

    исправление codestyle

--- src/task13/Recommendations.java
+++ src/task13/Recommendations.java
@@ -1,14 +1,11 @@
 package task13;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Recommendations {
-    private Map<Integer, String> films;
-    private List<Set<Integer>> preferencesBase;
-    private Set<Integer> user;
+    private final Map<Integer, String> films;
+    private final List<Set<Integer>> preferencesBase;
+    private final Set<Integer> user;
 
     public Recommendations(Map<Integer, String> films, List<Set<Integer>> preferencesBase, Set<Integer> user) {
         this.films = films;
@@ -16,22 +13,22 @@ public class Recommendations {
         this.user = user;
     }
 
-    public String getRecommendation(){
-        for(int i=0; i<preferencesBase.size(); i++){
+    public String getRecommendation() {
+        for (int i = 0; i < preferencesBase.size(); i++) {
             Set<Integer> pref = Collections.singleton(preferencesBase.indexOf(i));
-            if(intersection(pref)){
-                for(Integer number: pref){
-                    if(!user.contains(number)) return films.get(number);
+            if (intersection(pref)) {
+                for (Integer number : pref) {
+                    if (!user.contains(number)) return films.get(number);
                 }
             }
         }
         return "nothing";
     }
 
-    private boolean intersection(Set<Integer> preference){
-        int k=0;
-        for(Integer number: preference){
-            if(user.contains(number)) k++;
+    private boolean intersection(Set<Integer> preference) {
+        int k = 0;
+        for (Integer number : preference) {
+            if (user.contains(number)) k++;
         }
         return k >= user.size() / 2;
     }
