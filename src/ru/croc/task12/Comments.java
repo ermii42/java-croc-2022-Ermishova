a80d2862e20ac1716070bdf8cbc5d76927d6eb04
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-24 18:28:00 +0300

    добавлены комментарии и исправлен codestyle в 12 задаче

--- src/ru/croc/task12/Comments.java
+++ src/ru/croc/task12/Comments.java
@@ -8,22 +8,24 @@ import java.util.Set;
 public class Comments implements BlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
+        // список комментариев, которые необходимо удалить после фильтрации
         List<String> deletedComments = new ArrayList<>();
         for (String comment : comments) {
             for (String badWord : blackList) {
+                // проверка, является ли плохое слово подсловом другого или нет (однокоренные слова не учитываются)
                 if (comment.toLowerCase(Locale.ROOT).contains(badWord.toLowerCase(Locale.ROOT))) {
                     int index = comment.toLowerCase(Locale.ROOT).indexOf(badWord.toLowerCase(Locale.ROOT));
-                    if (index == 0 && index + badWord.length() <= comment.length()) {
-                        if (!Character.isLetter(comment.charAt(index + badWord.length()))) {
+                    boolean b1 = index + badWord.length() <= comment.length();
+                    boolean b2 = !Character.isLetter(comment.charAt(index + badWord.length()));
+                    if (index == 0 && b1) {
+                        if (b2) {
                             deletedComments.add(comment);
                         }
-                    } else if(index != 0 && index + badWord.length() <= comment.length()){
-                        if (!Character.isLetter(comment.charAt(index - 1))
-                                && !Character.isLetter(comment.charAt(index + badWord.length()))) {
+                    } else if (index != 0 && b1) {
+                        if (!Character.isLetter(comment.charAt(index - 1)) && b2) {
                             deletedComments.add(comment);
                         }
-                    }
-                    else{
+                    } else {
                         deletedComments.add(comment);
                     }
                 }
