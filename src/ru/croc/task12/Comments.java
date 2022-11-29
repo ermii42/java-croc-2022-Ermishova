ade07e35cea3dc0d7dc469867d3bc95d9d07fc33
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-11-29 20:37:42 +0300

    исправление выхода из циклов

--- src/ru/croc/task12/Comments.java
+++ src/ru/croc/task12/Comments.java
@@ -1,22 +1,20 @@
 package ru.croc.task12;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 
 public class Comments implements BlackListFilter {
     @Override
     public void filterComments(List<String> comments, Set<String> blackList) {
         // список комментариев, которые необходимо удалить после фильтрации
         List<String> deletedComments = new ArrayList<>();
+        metka:
         for (String comment : comments) {
             for (String badWord : blackList) {
                 // проверка, есть ли в строке плохие слова
                 for (String elem : comment.toLowerCase(Locale.ROOT).split("[^A-Za-zА-Яа-я]+")) {
                     if (elem.equals(badWord)) {
                         deletedComments.add(comment);
-                        break;
+                        continue metka;
                     }
                 }
             }
