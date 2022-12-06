dda37dd8f9fbd8f2d4dd3691048deef370ffb43b
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-12-06 21:25:12 +0300

    19 задача

--- src/ru/croc/task19/task19.java
+++ src/ru/croc/task19/task19.java
@@ -7,7 +7,7 @@ public class task19 {
         int k = 0;
         while (k < 1 << 5) {
             for (int i = 1; i < 72; i++) {
-                wind.loadImage("src/ru/croc/task19/sourse/" + Integer.toString(i) + ".png");
+                wind.loadImage("src/ru/croc/task19/sourse/" + i + ".png");
                 wind.drawIcon();
                 try {
                     Thread.sleep(50);
