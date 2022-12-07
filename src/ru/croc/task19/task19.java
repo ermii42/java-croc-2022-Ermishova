14b958ea3f8cada443a2f6fae4e7929125d8293e
cheshire cat <45429645+ermii42@users.noreply.github.com>
2022-12-08 01:39:39 +0300

    fix codestyle

--- src/ru/croc/task19/task19.java
+++ src/ru/croc/task19/task19.java
@@ -1,23 +1,19 @@
 package ru.croc.task19;
 
 public class task19 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         loadAnimation();
     }
 
-    public static void loadAnimation() {
+    public static void loadAnimation() throws InterruptedException {
         Window wind = new Window();
         int k = 0;
         while (k < 1 << 5) {
             for (int i = 1; i < 72; i++) {
                 wind.loadImage("src/ru/croc/task19/sourse/" + i + ".png");
                 wind.drawIcon();
-                try {
-                    Thread.sleep(50);
-                    k++;
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                Thread.sleep(50);
+                k++;
             }
         }
     }
